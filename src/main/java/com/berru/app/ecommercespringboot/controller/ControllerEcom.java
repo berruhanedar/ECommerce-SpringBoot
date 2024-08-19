c96c9358c373f4de8d4da6c9319afaca6dad0a79
ilaydagumustekin <ilaydagumustekin216@gmail.com>
2024-08-19 15:25:12 +0300

    test try

--- src/main/java/com/berru/app/ecommercespringboot/controller/ControllerEcom.java
+++ src/main/java/com/berru/app/ecommercespringboot/controller/ControllerEcom.java
@@ -1,4 +1,13 @@
 package com.berru.app.ecommercespringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerEcom {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
