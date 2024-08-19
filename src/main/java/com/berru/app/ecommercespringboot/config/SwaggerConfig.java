4f0c9592fc6054bf5867fe04349ce6829622ad07
berru <berruhanedar@icloud.com>
2024-08-19 13:36:44 +0300

    update

--- src/main/java/com/berru/app/ecommercespringboot/config/SwaggerConfig.java
+++ src/main/java/com/berru/app/ecommercespringboot/config/SwaggerConfig.java
@@ -1,2 +1,39 @@
-package com.berru.app.ecommercespringboot.config;public class SwaggerConfig {
+package com.berru.app.ecommercespringboot.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.berru.app.ecommercespringboot"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiInfoMetaData());
+    }
+
+
+    private ApiInfo apiInfoMetaData() {
+        return new ApiInfoBuilder().title("Tutorials")
+                .description("API Endpoint Decoration")
+                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 }
--- src/test/java/com/berru/app/ecommercespringboot/ECommerceSpringBootApplicationTests.java
+++ src/test/java/com/berru/app/ecommercespringboot/ECommerceSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ECommerceSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
