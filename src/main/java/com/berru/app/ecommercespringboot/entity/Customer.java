60ddec4fc4612f0242b89cae28ad399131e14364
ilaydagumustekin <ilaydagumustekin216@gmail.com>
2024-08-20 11:44:11 +0300

    update

--- src/main/java/com/berru/app/ecommercespringboot/entity/Customer.java
+++ src/main/java/com/berru/app/ecommercespringboot/entity/Customer.java
@@ -2,8 +2,13 @@ package com.berru.app.ecommercespringboot.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "customers")
 public class Customer {
@@ -34,67 +39,4 @@ public class Customer {
     @Column(name = "balance", nullable = false)
     private BigDecimal balance;
 
-    public Integer getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Integer customerId) {
-        this.customerId = customerId;
-    }
-
-    public Integer getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(Integer addressId) {
-        this.addressId = addressId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(Integer mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
