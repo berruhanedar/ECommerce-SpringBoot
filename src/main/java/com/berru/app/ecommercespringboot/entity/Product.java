e17e1fa3eae0673e5840448eba3405502925c7f4
berru <berruhanedar@icloud.com>
2024-08-22 11:06:23 +0300

    update product and productDTO

--- src/main/java/com/berru/app/ecommercespringboot/entity/Product.java
+++ src/main/java/com/berru/app/ecommercespringboot/entity/Product.java
@@ -1,13 +1,7 @@
 package com.berru.app.ecommercespringboot.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.Data;
 import java.math.BigDecimal;
 
 /**
@@ -15,31 +9,17 @@ import java.math.BigDecimal;
  */
 
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
-    @NotNull
-    @Size(min = 3, max = 30, message = "Product name size should be between 3-30")
     private String name;
-
-    @NotNull
-    @DecimalMin(value = "0.00")
     private BigDecimal price;
-
-    @Size(max = 255)
-    @NotNull
     private String description;
-
-    @NotNull
-    @Min(value = 0)
     private Integer quantity;
-
-    @Size(max = 255)
     private String image;
 
     @ManyToOne(fetch = FetchType.LAZY)
