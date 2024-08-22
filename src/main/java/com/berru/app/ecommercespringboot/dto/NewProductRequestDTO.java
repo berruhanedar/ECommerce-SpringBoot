4bcf3a65ffe4440861b97924e901319175be3554
berru <berruhanedar@icloud.com>
2024-08-22 11:26:17 +0300

    update NewProductRequestDTO

--- src/main/java/com/berru/app/ecommercespringboot/dto/NewProductRequestDTO.java
+++ src/main/java/com/berru/app/ecommercespringboot/dto/NewProductRequestDTO.java
@@ -1,21 +1,30 @@
 package com.berru.app.ecommercespringboot.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-
+import jakarta.validation.constraints.*;
+import lombok.Data;
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class NewProductRequestDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 30, message = "Product name size should be between 3-30")
     private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
     private BigDecimal price;
+
+    @NotBlank
+    @Size(max = 255)
     private String description;
+
+    @NotNull
+    @Min(value = 0)
     private Integer quantity;
+
+    @Size(max = 255)
     private String image;
+
     private Integer categoryId;
 }
