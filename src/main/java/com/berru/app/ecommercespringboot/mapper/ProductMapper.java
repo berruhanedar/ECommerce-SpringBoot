2465e55bda70f81d2241d0ad65aa663a45697366
berru <berruhanedar@icloud.com>
2024-08-22 16:52:27 +0300

    update ProductMapper

--- src/main/java/com/berru/app/ecommercespringboot/mapper/ProductMapper.java
+++ src/main/java/com/berru/app/ecommercespringboot/mapper/ProductMapper.java
@@ -4,12 +4,9 @@ import com.berru.app.ecommercespringboot.dto.NewProductRequestDTO;
 import com.berru.app.ecommercespringboot.dto.ProductDTO;
 import com.berru.app.ecommercespringboot.dto.UpdateProductRequestDTO;
 import com.berru.app.ecommercespringboot.entity.Product;
-import com.berru.app.ecommercespringboot.entity.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
-
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -18,22 +15,10 @@ public interface ProductMapper {
     @Mapping(target = "categoryId", source = "category.id")
     ProductDTO toDto(Product product);
 
-    //@Mapping(target = "category", source = "categoryId", qualifiedByName = "mapCategory")
     Product toEntity(NewProductRequestDTO dto);
 
-    //@Mapping(target = "category", source = "categoryId", qualifiedByName = "mapCategory")
     void updateProductFromDto(UpdateProductRequestDTO dto, @MappingTarget Product product);
 
     List<ProductDTO> toDtoList(List<Product> products);
 
-
-//    @Named("mapCategory")
-//    default Category mapCategory(Integer categoryId) {
-//        if (categoryId == null) {
-//            return null;
-//        }
-//        Category category = new Category();
-//        category.setId(categoryId);  // Set id directly
-//        return category;
-//    }
 }
