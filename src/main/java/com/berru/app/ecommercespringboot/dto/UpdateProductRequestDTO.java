c023cb1c6c8cdd4c314c08452f89033c145532f2
berru <berruhanedar@icloud.com>
2024-08-22 11:22:58 +0300

    update UpdateProductRequestDTOroduct

--- src/main/java/com/berru/app/ecommercespringboot/dto/UpdateProductRequestDTO.java
+++ src/main/java/com/berru/app/ecommercespringboot/dto/UpdateProductRequestDTO.java
@@ -1,22 +1,33 @@
 package com.berru.app.ecommercespringboot.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
+import jakarta.validation.constraints.*;
+import lombok.Data;
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class UpdateProductRequestDTO {
 
-    /**
-     * validation ekle
-     */
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 30, message = "Product name size should be between 3-30")
     private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
     private BigDecimal price;
+
+    @NotBlank
+    @Size(max = 255)
     private String description;
+
+    @NotNull
+    @Min(value = 0)
     private Integer quantity;
+
+    @Size(max = 255)
     private String image;
+
     private Integer categoryId;
     // status ekle
 }
