bad5fe8b68171c8e81807d68bc21ff51ba1deb35
berru <berruhanedar@icloud.com>
2024-08-22 16:59:45 +0300

    Update ProductService: Added @AllArgsConstructor annotation

--- src/main/java/com/berru/app/ecommercespringboot/service/ProductService.java
+++ src/main/java/com/berru/app/ecommercespringboot/service/ProductService.java
@@ -5,30 +5,25 @@ import com.berru.app.ecommercespringboot.entity.Category;
 import com.berru.app.ecommercespringboot.entity.Product;
 import com.berru.app.ecommercespringboot.exception.NotFoundException;
 import com.berru.app.ecommercespringboot.mapper.ProductMapper;
-
 import com.berru.app.ecommercespringboot.repository.CategoryRepository;
 import com.berru.app.ecommercespringboot.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @Service
+@AllArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
     private final CategoryRepository categoryRepository;
-
-    public ProductService(ProductRepository productRepository, ProductMapper productMapper, CategoryRepository categoryRepository) {
-        this.productRepository = productRepository;
-        this.productMapper = productMapper;
-        this.categoryRepository = categoryRepository;
-    }
 
     @Transactional
     public ProductDTO create(NewProductRequestDTO newProductRequestDTO) {
