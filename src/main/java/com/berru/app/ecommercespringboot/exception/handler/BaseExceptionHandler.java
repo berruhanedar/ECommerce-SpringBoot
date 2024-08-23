6f7be8fe5942d1f08b8d16cd7911b595a63761c3
veyselpehlivan <veysel.pehlivan@outlook.com>
2024-08-23 15:58:26 +0300

    checkstyle.xml and spotbugs are added

--- src/main/java/com/berru/app/ecommercespringboot/exception/handler/BaseExceptionHandler.java
+++ src/main/java/com/berru/app/ecommercespringboot/exception/handler/BaseExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class BaseExceptionHandler {
