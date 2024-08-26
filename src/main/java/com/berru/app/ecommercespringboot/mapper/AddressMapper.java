8715bcfdfc590833bc1a540994a7c8e0b42637be
ilaydagumustekin <ilaydagumustekin216@gmail.com>
2024-08-26 12:51:38 +0300

    delete instance in addressservice

--- src/main/java/com/berru/app/ecommercespringboot/mapper/AddressMapper.java
+++ src/main/java/com/berru/app/ecommercespringboot/mapper/AddressMapper.java
@@ -3,25 +3,15 @@ package com.berru.app.ecommercespringboot.mapper;
 
 import com.berru.app.ecommercespringboot.dto.AddressDTO;
 import com.berru.app.ecommercespringboot.dto.NewAddressRequestDTO;
-import com.berru.app.ecommercespringboot.dto.UpdateAddressRequestDTO;
 import com.berru.app.ecommercespringboot.entity.Address;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
 
-    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
 
     AddressDTO toAddressDTO(Address address);
 
-    AddressDTO toAddressDTO(NewAddressRequestDTO newAddressRequestDTO);
-
-    AddressDTO toAddressDTO(UpdateAddressRequestDTO updateAddressRequestDTO);
-
-    Address toAddress(AddressDTO addressDTO);
-
     Address toAddress(NewAddressRequestDTO newAddressRequestDTO);
-
-    Address toAddress(UpdateAddressRequestDTO updateAddressRequestDTO);
 }
--- src/main/java/com/berru/app/ecommercespringboot/service/AddressService.java
+++ src/main/java/com/berru/app/ecommercespringboot/service/AddressService.java
@@ -26,21 +26,23 @@ public class AddressService {
 
     @Transactional
     public AddressDTO createAddress(NewAddressRequestDTO newAddressRequestDTO) {
-        Address address = AddressMapper.INSTANCE.toAddress(newAddressRequestDTO);
+        Address address = addressMapper.toAddress(newAddressRequestDTO);
         Address savedAddress = addressRepository.save(address);
-        return AddressMapper.INSTANCE.toAddressDTO(savedAddress);
+        return addressMapper.toAddressDTO(savedAddress);
     }
 
     public AddressDTO getAddressById(Integer id) {
-        Optional<Address> address = addressRepository.findById(id);
-        return address.map(AddressMapper.INSTANCE::toAddressDTO).orElse(null);
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Address not found"));
+        return addressMapper.toAddressDTO(address);
     }
 
     public List<AddressDTO> getAllAddresses() {
         return addressRepository.findAll().stream()
-                .map(AddressMapper.INSTANCE::toAddressDTO)
+                .map(addressMapper::toAddressDTO)
                 .toList();
     }
+
 
     @Transactional
     public AddressDTO updateAddress(Integer id, UpdateAddressRequestDTO updateAddressRequestDTO) {
@@ -52,7 +54,7 @@ public class AddressService {
         address.setCity(updateAddressRequestDTO.getCity());
         address.setCountry(updateAddressRequestDTO.getCountry());
         Address updatedAddress = addressRepository.save(address);
-        return AddressMapper.INSTANCE.toAddressDTO(updatedAddress);
+        return addressMapper.toAddressDTO(updatedAddress);
     }
 
 
