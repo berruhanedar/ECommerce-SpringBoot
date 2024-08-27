a81f2f8bd8f0cccc7ea580d991f9904549530607
ilaydagumustekin <ilaydagumustekin216@gmail.com>
2024-08-27 22:50:08 +0300

    update import

--- src/main/java/com/berru/app/ecommercespringboot/entity/ShoppingCart.java
+++ src/main/java/com/berru/app/ecommercespringboot/entity/ShoppingCart.java
@@ -1,14 +1,19 @@
 package com.berru.app.ecommercespringboot.entity;
 
+import com.berru.app.ecommercespringboot.enums.ShoppingCartStatus;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Column;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+
+
 import lombok.Data;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -33,6 +38,13 @@ public class ShoppingCart {
     @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ShoppingCartItem> items = new ArrayList<>();
 
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private ShoppingCartStatus status;
+
     public void addItem(Product product, int quantity) {
         ShoppingCartItem item = new ShoppingCartItem();
         item.setProduct(product);
@@ -47,5 +59,27 @@ public class ShoppingCart {
         totalPrice = items.stream()
                 .map(item -> item.getPrice())
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+
+    public void checkout() {
+        BigDecimal calculatedTotalPrice = items.stream()
+                .map(item -> item.getProduct().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        this.totalPrice = calculatedTotalPrice;
+
+        if (address != null) {
+            System.out.println("Shipping Address:");
+            System.out.println("Street: " + address.getStreet());
+            System.out.println("Building Name: " + address.getBuildingName());
+            System.out.println("City: " + address.getCity());
+            System.out.println("Country: " + address.getCountry());
+            System.out.println("Postal Code: " + address.getPostalCode());
+        } else {
+            System.out.println("No address information available.");
+        }
+
+        this.status = ShoppingCartStatus.CHECKED_OUT;
     }
 }
--- src/main/java/com/berru/app/ecommercespringboot/service/ShoppingCartService.java
+++ src/main/java/com/berru/app/ecommercespringboot/service/ShoppingCartService.java
@@ -5,6 +5,7 @@ import com.berru.app.ecommercespringboot.dto.ShoppingCartDTO;
 import com.berru.app.ecommercespringboot.entity.Customer;
 import com.berru.app.ecommercespringboot.entity.Product;
 import com.berru.app.ecommercespringboot.entity.ShoppingCart;
+import com.berru.app.ecommercespringboot.enums.ShoppingCartStatus;
 import com.berru.app.ecommercespringboot.exception.ResourceNotFoundException;
 import com.berru.app.ecommercespringboot.mapper.ShoppingCartMapper;
 import com.berru.app.ecommercespringboot.repository.ProductRepository;
@@ -22,20 +23,15 @@ public class ShoppingCartService {
     private final ProductRepository productRepository;
 
     public void addToCart(Integer customerId, Integer productId, Integer quantity) {
-        // Sepeti müşteri ID'sine göre bul veya yeni sepet oluştur
         ShoppingCart cart = shoppingCartRepository.findByCustomerId(customerId)
                 .orElseGet(() -> createNewCartForCustomer(customerId));
 
-        // Ürünü ID'sine göre bul
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
 
-        // Sepete ürünü ekle
         cart.addItem(product, quantity);
         shoppingCartRepository.save(cart);
     }
-
-
 
     private ShoppingCart createNewCartForCustomer(Integer customerId) {
         ShoppingCart cart = new ShoppingCart();
@@ -45,7 +41,6 @@ public class ShoppingCartService {
         return shoppingCartRepository.save(cart);
     }
 
-
     @Transactional
     public ShoppingCartDTO getShoppingCartById(Integer id) {
         ShoppingCart shoppingCart = shoppingCartRepository.findById(id)
@@ -53,11 +48,20 @@ public class ShoppingCartService {
         return shoppingCartMapper.toDTO(shoppingCart);
     }
 
-
     @Transactional
     public void deleteShoppingCart(Integer id) {
         ShoppingCart shoppingCart = shoppingCartRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("ShoppingCart not found with id: " + id));
         shoppingCartRepository.delete(shoppingCart);
+    }
+
+    @Transactional
+    public void checkoutCart(Integer id) {
+        ShoppingCart shoppingCart = shoppingCartRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("ShoppingCart not found with id: " + id));
+
+        shoppingCart.setStatus(ShoppingCartStatus.CHECKED_OUT);
+        shoppingCart.checkout();
+        shoppingCartRepository.save(shoppingCart);
     }
 }
