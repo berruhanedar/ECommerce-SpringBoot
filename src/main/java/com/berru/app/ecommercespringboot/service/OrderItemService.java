4e1c3cdca6fa2065a9d1e1732726563e4d9fbaef
berru <berruhanedar@icloud.com>
2024-08-29 15:29:58 +0300

    add deleteOrderItem and updateOrderItem methods to OrderItemService

--- src/main/java/com/berru/app/ecommercespringboot/service/OrderItemService.java
+++ src/main/java/com/berru/app/ecommercespringboot/service/OrderItemService.java
@@ -1,6 +1,7 @@
 package com.berru.app.ecommercespringboot.service;
 
 import com.berru.app.ecommercespringboot.entity.OrderItem;
+import com.berru.app.ecommercespringboot.exception.ResourceNotFoundException;
 import com.berru.app.ecommercespringboot.repository.OrderItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,22 @@ public class OrderItemService {
 
     public void addOrderedProducts(OrderItem orderItem) {
         orderItemRepository.save(orderItem);
+    }
+
+    public void deleteOrderItem(Integer orderItemId) {
+        if (!orderItemRepository.existsById(orderItemId)) {
+            throw new ResourceNotFoundException("OrderItem not found with id: " + orderItemId);
+        }
+        orderItemRepository.deleteById(orderItemId);
+    }
+
+    public OrderItem updateOrderItem(OrderItem updatedOrderItem) {
+        return orderItemRepository.findById(updatedOrderItem.getOrderItemId())
+                .map(orderItem -> {
+                    orderItem.setQuantity(updatedOrderItem.getQuantity());
+                    orderItem.setOrderedProductPrice(updatedOrderItem.getOrderedProductPrice());
+                    return orderItemRepository.save(orderItem);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("OrderItem not found with ID: " + updatedOrderItem.getOrderItemId()));
     }
 }
