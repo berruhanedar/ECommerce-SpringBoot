853248b51783a0abd07a8d5c5b45e49d7399299e
berru <berruhanedar@icloud.com>
2024-09-02 11:32:48 +0300

    added validation annotations to UpdateOrderRequestDTO for order ID and total amount

--- src/main/java/com/berru/app/ecommercespringboot/dto/UpdateOrderRequestDTO.java
+++ src/main/java/com/berru/app/ecommercespringboot/dto/UpdateOrderRequestDTO.java
@@ -1,6 +1,8 @@
 package com.berru.app.ecommercespringboot.dto;
 
 import com.berru.app.ecommercespringboot.enums.OrderStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,15 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateOrderRequestDTO {
+
+    @NotNull(message = "Order ID cannot be null")
+    @Min(value = 1, message = "Order ID must be greater than 0")
     private Integer orderId;
+
     private OrderStatus orderStatus;
+
+    @Min(value = 0, message = "Total amount must be greater than or equal to 0")
     private BigDecimal totalAmount;
+
     private List<UpdateOrderItemRequestDTO> orderItems = new ArrayList<>();
 }
