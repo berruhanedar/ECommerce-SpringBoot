f73735fbe725ab45be958fdf968ceb5768962894
berru <berruhanedar@icloud.com>
2024-09-07 01:21:08 +0300

    add join query to `ShoppingCartRepository` for retrieving cart by customer ID

--- src/main/java/com/berru/app/ecommercespringboot/repository/ShoppingCartRepository.java
+++ src/main/java/com/berru/app/ecommercespringboot/repository/ShoppingCartRepository.java
@@ -3,13 +3,16 @@ package com.berru.app.ecommercespringboot.repository;
 import com.berru.app.ecommercespringboot.entity.Customer;
 import com.berru.app.ecommercespringboot.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
-    Optional<ShoppingCart> findById(Integer customerId);
+    @Query("SELECT sc FROM ShoppingCart sc JOIN sc.customer c WHERE c.id = :customerId")
+    Optional<ShoppingCart> findByCustomerId(@Param("customerId") Integer customerId);
 
     ShoppingCart findByCustomer(Customer customer);
 }
