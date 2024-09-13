a35fec3ee2b4e772433cdf65521686508f53e76d
berru <berruhanedar@icloud.com>
2024-09-13 10:08:25 +0300

    add Add JpaSpecificationExecutor for dynamic RSQL filtering in ProductRepository

--- src/main/java/com/berru/app/ecommercespringboot/repository/ProductRepository.java
+++ src/main/java/com/berru/app/ecommercespringboot/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.berru.app.ecommercespringboot.repository;
 import com.berru.app.ecommercespringboot.entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,9 +12,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
     List<Product> findByCategoryIdIn(List<Integer> categoryIds);
-
 
     @Query("SELECT p.quantity FROM Product p WHERE p.id = :productId")
     Optional<Integer> findStockByProductId(@Param("productId") Integer productId);
