1a0e5f4f5f9ab5a40054f6324d7a23b937f409f0
berru <berruhanedar@icloud.com>
2024-09-18 14:53:56 +0300

    update

--- src/main/java/com/berru/app/ecommercespringboot/service/KafkaConsumerService.java
+++ src/main/java/com/berru/app/ecommercespringboot/service/KafkaConsumerService.java
@@ -1,13 +1,17 @@
 package com.berru.app.ecommercespringboot.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
 
 @Service
+@Slf4j
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = "product-topic", groupId = "my-group")
-    public void consume(String message) {
-        System.out.println("Received message: " + message);
+    @KafkaListener(topics = "product-updates", groupId = "my-group")
+    public void consume(ConsumerRecord<String, String> record) {
+        log.info("Consumed message: Key = {}, Value = {}", record.key(), record.value());
     }
 }
