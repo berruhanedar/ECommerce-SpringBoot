e8c55b9a718ad15e451008b5bb37b4ad149b0842
berru <berruhanedar@icloud.com>
2024-09-29 00:00:38 +0300

    add validation annotations to Attribute entity

--- src/main/java/com/berru/app/ecommercespringboot/entity/Attribute.java
+++ src/main/java/com/berru/app/ecommercespringboot/entity/Attribute.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,9 @@ public class Attribute {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
     private String name;
 }
