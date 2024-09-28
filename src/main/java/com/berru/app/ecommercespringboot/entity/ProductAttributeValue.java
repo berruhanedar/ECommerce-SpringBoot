6656c82b20ced382872df694ac4b729feff61028
berru <berruhanedar@icloud.com>
2024-09-29 00:05:39 +0300

    add initial implementation of ProductAttributeValue entity

---
+++ src/main/java/com/berru/app/ecommercespringboot/entity/ProductAttributeValue.java
@@ -0,0 +1,40 @@
+package com.berru.app.ecommercespringboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductAttributeValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "attribute_id")
+    private Attribute attribute;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @NotNull
+    private String value;
+
+    public ProductAttributeValue(Attribute attribute, Product product, String value) {
+        this.attribute = attribute;
+        this.product = product;
+        this.value = value;
+    }
+}
