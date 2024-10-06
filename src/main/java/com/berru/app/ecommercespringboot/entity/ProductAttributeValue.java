42883dca1dfca0eb6d9f43b1ee5cf86abe4f59dc
berru <berruhanedar@icloud.com>
2024-10-06 20:19:24 +0300

    refactored ProductAttributeValue to use Object type for dynamic values

--- src/main/java/com/berru/app/ecommercespringboot/entity/ProductAttributeValue.java
+++ src/main/java/com/berru/app/ecommercespringboot/entity/ProductAttributeValue.java
@@ -1,15 +1,14 @@
 package com.berru.app.ecommercespringboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.berru.app.ecommercespringboot.converter.ObjectToStringConverter;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @Data
 @Entity
@@ -29,15 +28,34 @@ public class ProductAttributeValue {
     @JoinColumn(name = "product_id")
     private Product product;
 
-    /*
-    String yerine Object olmalı ve typelar belirtilmeli
-     */
     @NotNull
-    private String value;
+    @Convert(converter = ObjectToStringConverter.class) // Doğru dönüştürücü kullanıldığından emin olun
+    private Object value; // Dinamik değer
 
-    public ProductAttributeValue(Attribute attribute, Product product, String value) {
+    @NotNull
+    @Column(nullable = false, columnDefinition = "varchar(255)")
+    private String type; // Değerin türü
+
+    public ProductAttributeValue(Attribute attribute, Product product, Object value) {
         this.attribute = attribute;
         this.product = product;
         this.value = value;
+        this.type = determineValueType(value); // Girilen değere göre tür belirleniyor
+    }
+
+    private String determineValueType(Object value) {
+        if (value instanceof String) {
+            return "String";
+        } else if (value instanceof Integer) {
+            return "Integer";
+        } else if (value instanceof Double) {
+            return "Double";
+        } else if (value instanceof List) {
+            return "List";
+        } else if (value instanceof Date) {
+            return "Date"; // Tarih türü
+        } else {
+            return "Unknown"; // Bilinmeyen tür
+        }
     }
 }
